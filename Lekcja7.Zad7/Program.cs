8070d634c3cd82d3711074647e547868a96ae5b6
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 07:12:59 +0200

    Add project files.

---
+++ Lekcja7.Zad7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Pobieranie trzech liczb od użytkownika
+		Console.WriteLine("Podaj pierwszą liczbę:");
+		int liczba1 = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Podaj drugą liczbę:");
+		int liczba2 = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Podaj trzecią liczbę:");
+		int liczba3 = Convert.ToInt32(Console.ReadLine());
+
+		// Sprawdzanie, która liczba jest największa
+		int najwieksza = liczba1;
+		if (liczba2 > najwieksza)
+		{
+			najwieksza = liczba2;
+		}
+		if (liczba3 > najwieksza)
+		{
+			najwieksza = liczba3;
+		}
+
+		// Wyświetlanie wyniku
+		Console.WriteLine($"{najwieksza} jest największa z podanych.");
+	}
+}
